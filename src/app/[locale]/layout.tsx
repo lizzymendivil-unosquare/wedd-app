7a66ab0d765e599ff72b866f3d1eea323b5ed14a
lizzymendivil-unosquare <lizzy.mendivil@unosquare.com>
2023-11-27 16:28:52 -0400

    Remove head section

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,9 +39,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={inter.className}>
         {/* <div className="flex">
           <Navbar />
