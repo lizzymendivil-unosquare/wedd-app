aac02bd8c5ed532b56561a05eb425f4a4b351e76
lizzymendivil-unosquare <lizzy.mendivil@unosquare.com>
2023-11-27 16:23:50 -0400

    Remove useless code

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -11,9 +11,9 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    // }),
   ],
 } satisfies NextAuthOptions;
